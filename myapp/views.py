dd8b169e39ff65514e49ef403e13fc12119beb42
rahilyusuf <rahilyusuf.ry@gmail.com>
2024-06-23 22:40:39 +0530

    counter proj

--- myapp/views.py
+++ myapp/views.py
@@ -6,6 +6,11 @@ def index(request):
   'name':'rahil',
   'age':'21',
  }
- return render(request,'index.html', context)
+ return render(request,'index.html')
+
+def counter(request):
+ text = request.POST['text']
+ amount_of_words = len(text.split())
+ return render(request, 'counter.html', {'amount':amount_of_words} )
 
 # Create your views here.
